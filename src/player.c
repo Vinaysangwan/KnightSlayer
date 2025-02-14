14e1bec9bee7a7cbfb65bc8aeef10fb904a50f8d
Vinay Sangwan <pawansangwan541@gmail.com>
2025-02-15 00:16:36 +0530

    Some littler improvements

--- src/player.c
+++ src/player.c
@@ -7,20 +7,30 @@
 #define SPRITE_SIZE 16
 #define IDLE_ANIMATION_START_X 128
 #define RUN_ANIMATION_START_X 16
-#define FRAME_DELAY 2
-#define SCALE 1
+#define FRAME_DELAY 3
+#define SCALE 3
+
+typedef enum {
+    UP,
+    DOWN,
+    LEFT,
+    RIGHT
+}PlayerDirection;
+
+PlayerDirection direction = DOWN;
 
 SDL_Texture* player_texture = NULL;
 SDL_FRect sprite_position = {IDLE_ANIMATION_START_X,16,SPRITE_SIZE,SPRITE_SIZE};
 SDL_FRect player_position = {0,0,SCALE * SPRITE_SIZE, SCALE * SPRITE_SIZE};
 float frame_counter = 0;
-char direction = 's'; // Direction of the player Movement
 
 //****************************************************************************
 //************************* Play Player Animation ****************************
 //****************************************************************************
 void play_Player_Animation(int starting_position_x,int starting_position_y)
 {
+    frame_counter++;
+
     if (frame_counter/60 >= FRAME_DELAY)
     {
         frame_counter = 0;
@@ -28,8 +38,10 @@ void play_Player_Animation(int starting_position_x,int starting_position_y)
 
         if(sprite_position.x >= starting_position_x + SPRITE_SIZE * 5) {
             sprite_position.x = starting_position_x;
+            return;
         } else {
             sprite_position.x += SPRITE_SIZE;
+            return;
         }
     }
 }
@@ -39,7 +51,11 @@ void play_Player_Animation(int starting_position_x,int starting_position_y)
 //****************************************************************************
 void cleanup_Player()
 {
-
+    if(player_texture)
+    {
+        SDL_DestroyTexture(player_texture);
+        player_texture = NULL;
+    }
 }
 
 //****************************************************************************
@@ -56,27 +72,27 @@ void handle_Event_Player(SDL_Event* event)
 void update_Player(float delta_time)
 {
     const _Bool* keyboard_state = SDL_GetKeyboardState(NULL);
-    float velocity_x = 0 , velocity_y = 0;
+    float velocity_x = 0.f , velocity_y = 0.f;
 
     if(keyboard_state[SDL_SCANCODE_W])
     {
         velocity_y -= PLAYER_SPEED * delta_time;
-        direction = 'w';
+        direction = UP;
     }
     if(keyboard_state[SDL_SCANCODE_S])
     {
         velocity_y += PLAYER_SPEED * delta_time;
-        direction = 's';
+        direction = DOWN;
     }
     if(keyboard_state[SDL_SCANCODE_A])
     {
         velocity_x -= PLAYER_SPEED * delta_time;
-        direction = 'a';
+        direction = LEFT;
     }
     if(keyboard_state[SDL_SCANCODE_D])
     {
         velocity_x += PLAYER_SPEED * delta_time;
-        direction = 'd';
+        direction = RIGHT;
     }
 
     // Normalizing the velocity
@@ -86,18 +102,16 @@ void update_Player(float delta_time)
         velocity_y *= 0.7071f; 
     }
 
-    frame_counter++;
-
     // The Animation x coordinate
     int animation_start_x = (velocity_x == 0 && velocity_y == 0) ? IDLE_ANIMATION_START_X : RUN_ANIMATION_START_X;
     // The animation y coordinate
     int animation_start_y = 0;
 
     switch (direction) {
-        case 'w': animation_start_y = 64;break;
-        case 's': animation_start_y = 16;break;
-        case 'a': animation_start_y = 32;break;
-        case 'd': animation_start_y = 48;break;
+        case UP: animation_start_y = 64;break;
+        case DOWN: animation_start_y = 16;break;
+        case LEFT: animation_start_y = 32;break;
+        case RIGHT: animation_start_y = 48;break;
     }
 
     // Playing the animation
@@ -124,6 +138,10 @@ void init_Player(void *appstate)
 
     const char path[] = "./assets/Char_Sprites/char_spritesheet.png";
     player_texture = IMG_LoadTexture(state->renderer, path);
+    if(!player_texture)
+    {
+        SDL_Log("Error loading player texture: %s",SDL_GetError());
+    }
 
     // Making the texture pixelated(Better for pixel art)
     if(!SDL_SetTextureScaleMode(player_texture, SDL_SCALEMODE_NEAREST))
